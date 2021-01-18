0992a8cf469f69671af76d2d7ac4795bb818ca3a
DangDD <dn8118545@gmail.com>
2021-01-19 00:22:30 +0700

    upload cart.js -kiquy

---
+++ js/cart.js
@@ -0,0 +1,5 @@
+document.getElementById("myBtn1").addEventListener("click",  function.totalPrice1(){
+        var number1 = document.getElementById('1').value;
+        var result1 = parseFloat(number1) * 12999;
+        document.getElementById('result1').value = result1 ;
+});
